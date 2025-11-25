96da975cb0832636f14e1280824f6a675c0429fc
CelestePerez1 <Celeste.perez@utp.ac.pa>
2025-11-25 11:13:34 -0500

    Subiendo Base de datos y script

---
+++ DBAlumnos/DBAlumnos/SQLQuery1.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE DBAlumnos;
+
+CREATE TABLE Alumnos ( Id INT IDENTITY(1,1) PRIMARY KEY, 
+Nombre NVARCHAR(100), 
+Cedula NVARCHAR(20),
+Carrera NVARCHAR(50), 
+Semestre NVARCHAR(20), 
+Jornada NVARCHAR(20), 
+Usuario NVARCHAR(50),
+Contrasena NVARCHAR(100), 
+RecibirNotificaciones BIT, 
+FechaRegistro DATETIME DEFAULT GETDATE() );
+
+select * from Alumnos;
